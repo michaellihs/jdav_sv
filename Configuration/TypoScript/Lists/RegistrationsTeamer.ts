6c7133bdf775ea58abad8138efbb007e07c55003
Michael Knoll <mimi@kaktusteam.de>
2011-09-15 15:25:29 +0200

    ADD: Added fields to registrations list for teamers

--- Configuration/TypoScript/Lists/RegistrationsTeamer.ts
+++ Configuration/TypoScript/Lists/RegistrationsTeamer.ts
@@ -10,79 +10,137 @@
 
 plugin.tx_jdavsv.settings.listConfig.registrationsTeamer {
 
-    backendConfig < plugin.tx_ptextlist.prototype.backend.extbase
+    itemsPerPage = 0
+
+    backendConfig < plugin.tx_ptextlist.prototype.backend.typo3
     backendConfig {
-        repositoryClassName = Tx_JdavSv_Domain_Repository_RegistrationRepository
-        #sorting = title
+
+        tables (
+            tx_jdavsv_domain_model_registration registration,
+            tx_jdavsv_domain_model_event event,
+            fe_users
+        )
+
+        baseFromClause (
+            tx_jdavsv_domain_model_registration registration
+            LEFT JOIN tx_jdavsv_domain_model_event event ON registration.event = event.uid
+            LEFT JOIN fe_users ON registration.attendee = fe_users.uid
+        )
+
     }
-    
-    
-    
+
     fields {
-    
-        registration {
-            table = __self__
-            field = __object__
+        first_name {
+            table = fe_users
+            field = first_name
+            isSortable = 1
         }
 
-        date {
-            table = __self__
-            field = date
+        last_name {
+            table = fe_users
+            field = last_name
+            isSortable = 1
         }
 
-        attendee {
-            table = __self__
-            field = attendee
+        address {
+            table = fe_users
+            field = address
+            isSortable = 0
         }
 
-        reservedUntil {
-            table = __self__
-            field = reservedUntil
+        telephone {
+            table = fe_users
+            field = telephone
+            isSortable = 0
         }
 
-        waitingList {
-            table = __self__
-            field = waitingList
+        zip {
+            table = fe_users
+            field = zip
+            isSortable = 1
         }
 
-        registrationOrder {
-            table = __self__
-            field = registrationOrder
+        email {
+            table = fe_users
+            field = email
+            isSortable = 0
         }
 
-        vegetarian {
-            table = __self__
-            field = vegetarian
+        dateOfBirth {
+            special = '###TODO###'
+            # TODO Geburtsdatum muss als Feld zu fe_users hinzugefügt werden
         }
 
-        state {
-            table = __self__
-            field = state
+        sektion {
+            special = '###TODO###'
+            # TODO Sektion muss als Feld zu fe_users hinzugefügt werden
         }
 
-        paymentMethod {
-            table = __self__
-            field = paymentMethod
-        }
-
-        event {
-            table = __self__
-            field = event
-        }
-
-        isAccepted {
-            table = __self__
-            field = isAccepted
-        }
-    
     }
-    
-    
-    
-    columns {
-    
 
-    
+    columns {
+
+        10 {
+            label = Vor- und Nachname
+            fieldIdentifier = first_name, last_name
+            columnIdentifier = nameColumn
+        }
+
+        20 {
+            label = Adresse
+            fieldIdentifier = address, zip
+            columnIdentifier = addressColumn
+            sortingFields {
+                10 {
+                    field = zip
+                    direction = asc
+                    forceDirection = 0
+                    label = Adresse (nach PLZ sortieren)
+                }
+            }
+
+            renderObj = COA
+            renderObj {
+                10 = TEXT
+                10.data = field:address
+            }
+        }
+
+        30 {
+            label = Telefon
+            fieldIdentifier = telephone
+            columnIdentifier = telephoneColumn
+            isSortable = 0
+        }
+
+        40 {
+            label = E-Mail
+            fieldIdentifier = email
+            columnIdentifier = emailColumn
+            isSortable = 0
+        }
+
+        50 {
+            label = Geburtsdatum
+            fieldIdentifier = dateOfBirth
+            columnIdentifier = dateOfBirthColumn
+            isSortable = 0
+        }
+
+        60 {
+            label = Sektion
+            fieldIdentifier = sektion
+            columnIdentifier = sektionColumn
+            isSortable = 1
+        }
+    }
+
+    filters {
+
+        filterbox1 {
+
+        }
+
     }
 
 }
