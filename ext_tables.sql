480f060a1a4d0a31f050b5017f7d7768473b32c4
Michael Knoll <mimi@kaktusteam.de>
2012-03-06 13:34:22 +0100

    ADD: Added prerequisites in category form

--- ext_tables.sql
+++ ext_tables.sql
@@ -109,6 +109,7 @@ CREATE TABLE tx_jdavsv_domain_model_category (
 	name varchar(255) DEFAULT '' NOT NULL,
 	tour_report_required tinyint(1) unsigned DEFAULT '0' NOT NULL,
     shortcut varchar(255) DEFAULT '' NOT NULL,
+    prerequisites int(11) DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
